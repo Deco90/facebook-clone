8f1f65c0d1e9e524004513f83720c14d5a6ef472
bar <bardori@gmail.com>
2021-02-16 19:16:19 +0200

    first commit

---
+++ src/MessageSender.js
@@ -0,0 +1,49 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import "./MessageSender.css";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+
+function MessageSender() {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefualt();
+  };
+
+  return (
+    <div className="messageSender">
+      <div className="messgeSender__top">
+        <Avatar />
+        <form>
+          <input
+            className="messangerSender__input"
+            placeholder={`What's on your mind`}
+          />
+          <input placeholder="image URL (optional)" />
+          <buuton onClick={handleSubmit} type="submit">
+            Hidden submit
+          </buuton>
+        </form>
+      </div>
+
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageSender;
